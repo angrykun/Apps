3aee9dab071d8ec514892977519c7b10d1334b19
angrykun <2545662859@qq.com>
2017-08-07 14:03:33 +0800

    创建项目及首页

---
+++ Apps/Apps.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,44 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Apps.Web
+{
+    public class BundleConfig
+    {
+        // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
+            // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            //easyui js 文件
+            bundles.Add(new ScriptBundle("~/bundles/easyui").Include(
+                      "~/Scripts/jquery.easyui-1.4.5.js",
+                      "~/Scripts/locale/easyui-lang-zh_CN.js"));
+            //home js 文件
+            bundles.Add(new ScriptBundle("~/bundles/home").Include(
+                      "~/Scripts/home.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            //easyui css 文件
+            bundles.Add(new StyleBundle("~/Content/easyuicss").Include(
+                       "~/Content/themes/default/easyui.css",
+                       "~/Content/themes/icon.css"));
+        }
+    }
+}
---
+++ Apps/Apps.Web/Scripts/home.js
@@ -0,0 +1,91 @@
+﻿$(function () {
+    //刷新
+    $("#tab_menu-tabrefresh").click(function () {
+
+        var iframe = $(".tabs-panels .panel").eq($(".tabs-selected").index()).find("iframe");
+        var url = iframe.prop("src");
+        iframe.prop("src", url);
+    });
+
+    //在新窗口打开
+    $("#tab_menu-openFrame").click(function () {
+        var iframe = $(".tabs-panels .panel").eq($(".tabs-selected").index()).find("iframe");
+        var url = iframe.prop("src");;
+        window.open(url);
+    });
+
+    //关闭所有
+    $("#tab_menu-tabcloseall").click(function () {
+        $(".tabs-inner span").each(function (index, value) {
+            if ($(this).parent().next().is(".tabs-close")) {
+                var t = $(value).text();
+                $('#mainTab').tabs('close', t);
+            }
+            //open menu
+            $(".layout-button-right").trigger("click");
+        });
+    });
+
+    //关闭其他标签
+    $("#tab_menu-tabcloseothers").click(function () {
+        var current_title = $(".tabs-selected .tabs-inner span").text();
+        $(".tabs-inner span").each(function (index, value) {
+            var title = $(value).text();
+            if (current_title != title) {
+                $("#mainTab").tabs("close", title);
+            }
+        });
+    });
+
+    //关闭左侧
+    $("#tab_menu-tabcloseleft").click(function () {
+        var prevAll = $(".tabs-selected").prevAll();
+        if (prevAll.length == 0) {
+            $.messager.alert('提示', '前面没有了', 'warning');
+            return false;
+        }
+        prevAll.each(function (index, value) {
+            var t = $(value).text();
+            $("#mainTab").tabs("close", t);
+        });
+    });
+
+    //关闭右侧
+    $("#tab_menu-tabcloseright").click(function () {
+        var nextAll = $(".tabs-selected").nextAll();
+        if (nextAll.length == 0) {
+            $.messager.alert('提示', '后面没有了', 'warning');
+            return false;
+        }
+        nextAll.each(function (index, value) {
+            var t = $(value).text();
+            $("#mainTab").tabs("close", t);
+        });
+    });
+
+    //关闭当前页
+    $("#tab_menu-tabclose").click(function () {
+        var current_title = $(".tabs-selected .tabs-inner span").text();
+        $("#mainTab").tabs("close", current_title);
+        if ($(".tabs li").length == 0) {
+            //open menu
+            $(".layout-button-right").trigger("click");
+        }
+    });
+});
+
+$(function () {
+    $(".tabs li").on("contextmenu", function (e) {
+        var subTitle = $(this).text();
+        $("#mainTab").tabs("select", subTitle);
+        $("#tab_menu").menu("show", {
+            top: e.pageY,
+            left: e.pageX
+        });
+        e.preventDefault();
+    });
+});
+
+function addTab() {
+
+}
